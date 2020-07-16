0992e702a7646195d3eb0f9cde1d156f9547f55d
Grégory Pyck <gregpyck.ephec@gmail.com>
2020-07-16 19:26:56 +0200

    mise en place d'une classe pour passer en oriente objet

--- getinfo.py
+++ getinfo.py
@@ -1,22 +1,31 @@
 #utilisation du module os pour pouvoir interagir avec le systeme.
 
 import os
-from pathlib import Path
-
-#afficher des informations sur le systeme
-infoos = os.uname()
-print "Informations du systeme :\n {} \n".format(infoos)
-
-#afficher l'utilisateur actuel
-currentUser = os.getlogin()
-print "Current user :\n {} \n".format(currentUser)
-
-#afficher le path du repertoire actuel
-rep_actuel = os.getcwd()
-print "Repertoire actuel :\n {} \n".format(rep_actuel)
 
 
-myDirectory="C:/Users/"
-p = Path(myDirectory)
-for x in os.scandir(p):
-    print(x)
+#creation d'une classe pour rendre le programme oriente objet
+class GetInfo:
+    infos = os.uname()
+    currentUser = os.getlogin()
+    rep_actuel = os.getcwd()
+
+    def listInfo(self):
+        #afficher des informations sur le systeme
+        print "Informations du systeme :{} ".format(GetInfo.infos)
+        #afficher l'utilisateur actuel
+        print "Current user : {} \n".format(GetInfo.currentUser)
+        #afficher le path du repertoire actuel
+        print "Repertoire actuel : {} \n".format(GetInfo.rep_actuel)
+
+
+    def scan():
+        myDirectory="C:/Users/"
+        p = Path(myDirectory)
+        for x in os.scandir(p):
+            print(x)
+
+
+#instance de l'objet GetInfo (creation de l'objet)
+GetMyInfo = GetInfo()
+
+GetMyInfo.listInfo()
