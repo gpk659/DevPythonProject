f0f57c6992ab787b07384ebe6707268db65c16f0
Grégory Pyck <gregpyck.ephec@gmail.com>
2020-08-09 20:42:00 +0200

    ajustement du code pour getinfo.py

--- getinfo.py
+++ getinfo.py
@@ -1,13 +1,14 @@
 #utilisation du module os pour pouvoir interagir avec le systeme.
 
-import os
-
+import os,sys
 
 #creation d'une classe pour rendre le programme oriente objet
 class GetInfo:
     infos = os.uname()
     currentUser = os.getlogin()
     rep_actuel = os.getcwd()
+    myDirectory = os.listdir("/Users/")
+    scanpath = "Liste de tous les repertoires dans Users : \n"
 
     def listInfo(self):
         #afficher des informations sur le systeme
@@ -17,15 +18,17 @@ class GetInfo:
         #afficher le path du repertoire actuel
         print "Repertoire actuel : {} \n".format(GetInfo.rep_actuel)
 
+    def scan(self):
+        print "Liste de tous les repertoires dans Users/"
+        #on liste toutes les directories et sous directories
+        for file in GetInfo.myDirectory:
+            GetInfo.scanpath += file + "; \n"
+        print GetInfo.scanpath
 
-    def scan():
-        myDirectory="C:/Users/"
-        p = Path(myDirectory)
-        for x in os.scandir(p):
-            print(x)
 
 
 #instance de l'objet GetInfo (creation de l'objet)
 GetMyInfo = GetInfo()
-
+# afficage des infos de l'objet
 GetMyInfo.listInfo()
+GetMyInfo.scan()
